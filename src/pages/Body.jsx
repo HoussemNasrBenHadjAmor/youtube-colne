69371f36d9d7ef22ef082b3fc23012a160738e68
HoussemNasrBenHadjAmor <houssem.nasrbenhadjamor@essths.u-sousse.tn>
2023-06-16 22:44:20 -0400

    Trying to do the on the scroll page loading

--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -1,21 +1,55 @@
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { usefetchSearch } from "../lib/transtackReactQuery";
+import { fetchSearch } from "../lib/ApiFetch";
 
+import InfiniteScroll from "react-infinite-scroller";
 import { Videos, HomeLoader, ErrorPage } from "../components";
 
 const Body = () => {
   const { pathname } = useLocation();
   const query = pathname.slice(10).length ? pathname.slice(10) : "new";
 
-  const { data, status, isLoading } = usefetchSearch(query, 50);
+  // const { data, status, isLoading } = usefetchSearch(query, 1);
 
-  return isLoading ? (
-    <HomeLoader grid />
-  ) : status === "error" ? (
+  const [data, setData] = useState([]);
+  const [status, setStatus] = useState("");
+
+  const fetchSearchFun = async (query, number) => {
+    await fetchSearch(query, number)
+      .then(({ items }) => {
+        setData(...data, items);
+      })
+      .catch(() => {
+        setStatus("error");
+      });
+  };
+
+  const isError = status === "error";
+
+  console.log("data", data);
+
+  // return isLoading ? (
+  //   <HomeLoader grid />
+  // ) : status === "error" ? (
+  //   <ErrorPage />
+  // ) : (
+  //   <div>
+  //     <Videos videos={data} />
+  //   </div>
+  // );
+  return isError ? (
     <ErrorPage />
   ) : (
     <div>
-      <Videos videos={data} />
+      <InfiniteScroll
+        loadMore={fetchSearchFun(query, 2)}
+        hasMore={true}
+        loader={<HomeLoader grid key={0} />}
+        initialLoad={true}
+      >
+        <Videos videos={data} />
+      </InfiniteScroll>
     </div>
   );
 };
